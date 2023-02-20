3fbf9c1aedef4c9b74446333264815b352bc4d14
Shavak <shavakchauhan27@gmail.com>
2023-02-20 11:13:25 +0530

    adding files

--- deployment/app.py
+++ deployment/app.py
@@ -1,51 +1,50 @@
-from flask import Flask, Response, request
-import json
-from NamedEntity import *
-import flask
-from tika import parser
-from werkzeug.utils import secure_filename
-import os
-import pandas as pd
+  from flask import Flask, Response, request
+  import json
+  from NamedEntity import *
+  import flask
+  from tika import parser
+  from werkzeug.utils import secure_filename
+  import os
+  import pandas as pd
 
 
-app = Flask(__name__)
-app.config['UPLOAD_PATH'] = 'uploads'
-app.config['UPLOAD_EXTENSIONS'] = ['pdf']
+  app = Flask(__name__)
+  app.config['UPLOAD_PATH'] = 'uploads'
+  app.config['UPLOAD_EXTENSIONS'] = ['pdf']
 
 
-def pdf_to_text(filepath):
-  '''Extracts the text data from the pdf'''
-  raw = parser.from_file(filepath)
-  text = raw['content']
-  return text
+  def pdf_to_text(filepath):
+    '''Extracts the text data from the pdf'''
+    raw = parser.from_file(filepath)
+    text = raw['content']
+    return text
 
-def preprocess(text):
-  text = "".join([s for s in text.splitlines(True) if s.strip("\r\n")])
-  # text = re.sub('[^A-Za-z0-9\n]+', ' ', text)
-  s = 1+1
-  return text
-
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
-
-@app.route("/predict_entity", methods=["POST"])
-def predict_entity():
-    f = request.files['file']
-    filename = secure_filename(f.filename)
-    f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    text = pdf_to_text('uploads/'+filename)    
-    entity_dict = NamedEntityService.get_entities(text)
-    df = pd.DataFrame(list(entity_dict.items()),columns=['text','entity'])
-    # df.to_csv('result.csv')
-    os.remove(os.path.join(app.config['UPLOAD_PATH'],filename))
-
-    return flask.render_template('result.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
-
-    # return Response(json.dumps(entity_dict), status=200, mimetype='application/json')
-    
+  def preprocess(text):
+    text = "".join([s for s in text.splitlines(True) if s.strip("\r\n")])
+    # text = re.sub('[^A-Za-z0-9\n]+', ' ', text)
+    return text
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port = 5000)
+  @app.route('/')
+  def index():
+      return flask.render_template('index.html')
+
+  @app.route("/predict_entity", methods=["POST"])
+  def predict_entity():
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+      text = pdf_to_text('uploads/'+filename)    
+      entity_dict = NamedEntityService.get_entities(text)
+      df = pd.DataFrame(list(entity_dict.items()),columns=['text','entity'])
+      # df.to_csv('result.csv')
+      os.remove(os.path.join(app.config['UPLOAD_PATH'],filename))
+
+      return flask.render_template('result.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+      # return Response(json.dumps(entity_dict), status=200, mimetype='application/json')
+      
+
+
+  if __name__ == "__main__":
+      app.run(host='0.0.0.0',port = 5000)
